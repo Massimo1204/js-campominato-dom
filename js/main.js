8121ff8e561ad1ff6edb6cc7da9bf556f8101113
Massimo1204 <xiangmassimo@gmail.com>
2022-03-07 23:54:46 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const mode = document.getElementById('mode');
 const gameField = document.getElementById('game-field');
-document.getElementById('play-button').addEventListener('click', startGame);
+const score = document.getElementById('score');
 let quantity = 16;
+
+document.getElementById('play-button').addEventListener('click', startGame);
 
 function startGame(){
     createField(chooseMode(mode.value));
@@ -22,8 +24,6 @@ function chooseMode(mode){
     }
     return fieldSize;
 }
-
-
 
 function randomNumber(spectrum , start){
     let num = (Math.floor(Math.random()*spectrum )+ start);
@@ -46,8 +46,10 @@ function bombSpots(quantity,size){
     return bombs;
 }
 
+
 function createField(size){
     gameField.innerHTML = '';
+    let scoreCounter=0;
     const bombs = bombSpots(quantity,size);
     for(let i=0;i<size;i++){
         const newBox = document.createElement('div');
@@ -57,11 +59,17 @@ function createField(size){
         newBox.innerHTML= i+1;
         gameField.appendChild(newBox);
         newBox.addEventListener('click', function(){
-            if(!bombs.includes(parseInt(this.innerHTML))){
-                this.classList.add('safe');
+            console.log(this);
+            if(!bombs.includes(parseInt(newBox.innerHTML))){
+                newBox.classList.add('safe');
+                scoreCounter++;
+                score.innerHTML = "Punteggio : <span class='fw-bold fs-1 text-primary'> "+ scoreCounter +"</span>" ;
             }
             else{
-                this.classList.add('bomb');
+                for(let i=0;i<bombs.length;i++){
+                    document.querySelector("#game-field .box:nth-child("+bombs[i]+")").classList.add('bomb');
+                }
+                score.innerHTML = "<span class='fw-bold fs-2 text-danger'>Hai Perso!</span><br> Il tuo punteggio è :<span class='fw-bold fs-1 text-primary'> " + scoreCounter+"</span>" ;
             }
         });
     }
